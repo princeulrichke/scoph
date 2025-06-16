be5ee1767906367633da7c3061d4e56d61357234
Prince Ulrich Abel <princeulrich254@gmail.com>
2025-06-16 10:02:12 +0300

    Update page.tsx

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -1,12 +1,14 @@
+import { Suspense } from 'react';
 import { About, Mission, CoreValues, Affiliations, CTA } from "@/components";
+
 export default function Page() {
     return (
-        <>
+        <Suspense fallback={<div className="flex items-center justify-center min-h-screen">Loading...</div>}>
             <About />
             <Mission />
             <CoreValues />
             <Affiliations />
             <CTA />
-        </>
+        </Suspense>
     );
 }
